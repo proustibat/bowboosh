5d003051131391149109070a16e165a580058ab1
Proustibat <jennifer.proust@gmail.com>
2016-05-11 21:48:51 +0200

    add run-help task and add documentation to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 "use strict";
 
-var config = require( './config.json' );
+/**
+ * Configuration files
+ */
+var config = require( './config.json' ),
+    pkg = require('./package.json'),
+    bwr = require('./bower.json');
 
-var gulp = require( 'gulp' ),
+/**
+ * Plugins
+ */
+var gulp = require('gulp-help')(require('gulp')),
     sass = require( 'gulp-sass' ),
     notify = require( "gulp-notify" ),
     bower = require( 'gulp-bower' ),
@@ -10,12 +18,12 @@ var gulp = require( 'gulp' ),
     uglify = require( 'gulp-uglify' ),
     concat = require( 'gulp-concat' );
 
+
 /**
  * Runs bower install including in the gulpfile,
  * contributors only have to run gulp bower and have
  * them all setup and ready
  */
-
 gulp.task( 'bower', function () {
         return bower()
             .pipe( gulp.dest( config.bowerDir ) );
@@ -24,7 +32,7 @@ gulp.task( 'bower', function () {
 
 
 /**
- * Copy fonts from fontawesome to public directory
+ * Copy icons font from font-awesome to public directory
  */
 gulp.task( 'icons', function () {
         return gulp.src( config.bowerDir + '/font-awesome/fonts/**.*' )
@@ -32,7 +40,9 @@ gulp.task( 'icons', function () {
     }
 );
 
-// copy bootstrap required fonts to destination
+/**
+ * Copy bootstrap required fonts
+ */
 gulp.task( 'fonts', function () {
         return gulp
             .src( [
@@ -46,7 +56,9 @@ gulp.task( 'fonts', function () {
 
 
 /**
- * Copy sass files for theme
+ * Copy sass files for theme.
+ * Theme name is defined in config file.
+ * All themes are downloaded with bower with bootswatch
  */
 gulp.task( 'bootswatch-theme', function () {
         return gulp.src( config.bowerDir + '/bootswatch/'+ config.bootstrapTheme +'/*.scss' )
@@ -54,7 +66,15 @@ gulp.task( 'bootswatch-theme', function () {
     }
 );
 
-// compile scss
+
+/**
+ * Compile scss files
+ *  - bootstrap
+ *  - font-awesome
+ *  - custom scss files imported in main.scss
+ *  If bower components are added in the project and needs
+ *  css files, add path in includePaths, then import in main.scss
+ */
 gulp.task( 'sass', [ 'bootswatch-theme', 'fonts', 'icons' ], function () {
         return gulp.src( config.srcPath + '/css/main.scss' )
             .pipe( sass( {
@@ -71,7 +91,10 @@ gulp.task( 'sass', [ 'bootswatch-theme', 'fonts', 'icons' ], function () {
     }
 );
 
-// compile bower javascript files
+/**
+ * Compile libraries used in the project
+ * Main bower files are overrided in bower.json
+ */
 gulp.task( 'vendorjs', function () {
         return gulp.src( mainBowerFiles('**/*.js'), { base: config.bowerDir } )
             .pipe( concat( 'libs.js' ) )
@@ -81,7 +104,9 @@ gulp.task( 'vendorjs', function () {
 );
 
 
-// default task
+/**
+ * Default task when running 'gulp' command mine
+ */
 gulp.task( 'default', [ 'sass', 'vendorjs' ], function () {
         gulp.watch( config.srcPath + '/' + config.cssDir + '/**/*.scss', [ 'sass' ] );
         gulp.watch( config.bowerDir + '/**/*.js', [ 'vendorjs' ] );
